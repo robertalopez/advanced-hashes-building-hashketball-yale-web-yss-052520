3634fba3a67a5102258805e0889db50d4138f5b9
Robert Lopez <robert.lopez@yale.edu>
2020-05-15 05:23:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,26 @@
 # Write your code here!
 def game_hash
   hash = {
-    :home => {},
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"], 
+      :players => [
+        {:player_name => "Alan Anderson", 
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3, 
+          :blocks => 1, 
+          :slam_dunks => 1
+        },
+        {:player_name => "Reggie Evans",
+        :number =>30,
+        :shoe 
+          
+        }]
+    },
     :away => {}
   }
 end 
