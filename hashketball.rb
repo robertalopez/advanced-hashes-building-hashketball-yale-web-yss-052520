10a6019e4e222eaf000e4fea6f3ca8c3970d07ba
Robert Lopez <robert.lopez@yale.edu>
2020-05-15 05:39:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -61,6 +61,16 @@ def game_hash
       :team_name => "Charlotte Hornets",
       :colors => ["Turqoise", "Purple"],
       :players => [
+        {:player_name => "Jeff Adrien",
+         :number => 4,
+         :shoe => 18,
+         :points => 10,
+         :rebounds => 1,
+         :assists => 1,
+         :steals => 2,
+         :blocks => 7,
+         :slam_dunks => 2 
+        },
         {
           
         }]
