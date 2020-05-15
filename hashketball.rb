540096746e39b6666715882d87383237acf001ed
Robert Lopez <robert.lopez@yale.edu>
2020-05-15 05:28:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,9 +18,14 @@ def game_hash
         {:player_name => "Reggie Evans",
         :number =>30,
         :shoe => 14,
-        
-          
-        }]
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12, 
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
+        },
+        {}]
     },
     :away => {}
   }
